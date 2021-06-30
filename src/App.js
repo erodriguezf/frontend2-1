cedd8a423cb0852de7e525a5059337a95a1e9a83
elkinrodriguez <erodriguezf@uninorte.edu.co>
2021-06-30 12:20:27 -0500

    tercer cambio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import Concepto from './componentes/Concepto'
 
-function App() {
-  return (
-    <div className="App">
-      
-       <p>Hola mundo 1 </p>
-      
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <div class="row">
+        <div class="col-md-2" style={{width:120, height:120}} >
+          <img src="https://hypernovalabs.hiringroom.com/data/accounts/hypernovalabs/microsite/53da3eccad4c7886684d879cffb17504.png" 
+          style={{width:100, height:100}}></img>
+        </div>
+         <div class="col-md-16" style={{marginTop:45,width:350, height:80}}>
+           <p style={{fontFamily:'Arial', fontSize: 35}}>
+               Reporte de Gastos
+           </p>
+         </div>
+        </div>
+
+        <Concepto>
+
+        </Concepto>
+
+     
+        
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/componentes/Concepto.js
@@ -0,0 +1,13 @@
+import  React from 'react';
+class Concepto extends React.Component{
+    render(){
+        return(
+            <div class="row">
+  <div class="col-md-8">.col-md-8</div>
+  <div class="col-md-4">.col-md-4</div>
+</div>
+        )
+    }
+}
+
+export default Concepto;
